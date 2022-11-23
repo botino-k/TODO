ba5090a07da43b721771569eecb1c37f027a1f83
botino-k <lassmary@bk.ru>
2022-11-23 17:12:29 +0400

    init: addless for CRA

---
+++ my-app/config-overrides.js
@@ -0,0 +1,10 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { override } = require('customize-cra');
+const addLessLoader = require('customize-cra-less-loader');
+module.exports = override(
+  addLessLoader({
+    lessOptions: {
+      javascriptEnabled: true,
+    },
+  })
+);
